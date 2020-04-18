91d3a600d5e76aacc728e7ce08bfd1241ee982e8
Arda Çebi <ardacebi1@gmail.com>
2020-04-19 00:11:02 +0300

    Organize Automization scripts

--- classic/MerPlaces/change.sh
+++ classic/MerPlaces/change.sh
@@ -1,5 +1,5 @@
 for x in $*
 do
-sed -e "s/#ff6600/#ff4d0d/g" $x > temp$x
+sed -e "s/#000000/#842609/g" $x > temp$x
 mv temp$x $x
 done
--- dark/Papirus-Mer/MerPlaces/change.sh
+++
@@ -1,5 +0,0 @@
-for x in $*
-do
-sed -e "s/#ff6600/#ff4d0d/g" $x > temp$x
-mv temp$x $x
-done
